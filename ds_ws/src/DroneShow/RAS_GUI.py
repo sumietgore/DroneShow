801e02f1b6428b9d368477c76f1802ced3cd1ab1
Nina1031 <73870119+Nina1031@users.noreply.github.com>
2022-06-04 11:20:10 +0300

    Update RAS_GUI.py

--- ds_ws/src/DroneShow/RAS_GUI.py
+++ ds_ws/src/DroneShow/RAS_GUI.py
@@ -1,14 +1,16 @@
 import PySimpleGUI as sg
 from threading import Thread
-import threading
 import os
 # import random
 import csv
+import matplotlib, time
+
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
 # SuperFastPython.com
 # example of returning multiple value from a thread
 from time import sleep
-from threading import Thread
 
 # custom thread
 # class DroneThread(Thread):
@@ -48,11 +50,31 @@ def readCSV(csv_path):
     # global x = 0
     # global y = 0
     # read csv
-    with open(filename, "r") as infile:
-        csvreader = csv.reader(infile)
-        for row in csvreader:
-            x1,y1 = row[0],row[1]
-            print("x1,y1",x1,y1)
+    try:
+        with open(filename, "r") as infile:
+            csvreader = csv.reader(infile)
+            for line in csvreader:
+                x1,y1 = line[0],line[1]
+                print("x1,y1",x1,y1)
+                return x1,y1
+    except Exception as e:
+        print('CSV READ ERROR')
+
+
+def delete_fig_agg(fig_agg):
+    fig_agg.get_tk_widget().forget()
+    plt.close('all')
+
+def drone_show(window,x,y):  # this should be called as a thread, then time.sleep() here would not freeze the GUI
+
+    graph = window('graph')
+    print(x,y)
+    graph.draw_circle((x, y), 10, fill_color='blue', line_color='white')
+    window.refresh()
+    time.sleep(1)
+    # return plt.gcf()
+
+
 
 def LEDIndicator(key=None, radius=30):
     return sg.Graph(canvas_size=(radius, radius),
@@ -71,54 +93,42 @@ def inSafeArea(x,y):
     else:
         return False
 
-# drones initial location
-x1,y1 = 50,400
-x2,y2 = 150,400
-x3,y3 = 250,400
-x4,y4 = 350,400
 
+def gui():
 
-def main():
-
-
-
-    cwd=os.getcwd()
+    cwd = os.getcwd()
 
     AREADATAINPUT = [
-            [sg.Text('Input CSV path', font=('Arial', 14)),sg.InputText('CSV path',key='-CSV-', do_not_clear=True,size=(30, 3)),sg.FileBrowse(initial_folder=cwd)],
-            [sg.Button('START', font=('Arial', 10), size=(10, 3))]
+        [sg.Text('Input CSV path', font=('Arial', 14)),
+         sg.InputText('CSV path', key='-CSV-', do_not_clear=True, size=(30, 3)), sg.FileBrowse(initial_folder=cwd)],
+        [sg.Button('START', font=('Arial', 10), size=(10, 3))]
     ]
 
     AREADRONESHOW = [
         [sg.Graph(
-        canvas_size=(400, 400),
-        graph_bottom_left=(0, 0),
-        graph_top_right=(800, 800),
-        key="-GRAPH-",
-        #enable_events=True,
-        background_color='lightblue')]]
+            canvas_size=(400, 400),
+            graph_bottom_left=(0, 0),
+            graph_top_right=(800, 800),
+            key="-graph-",
+            # enable_events=True,
+            background_color='lightblue')]]
     AREALEFT = [
         [sg.Frame(layout=[[sg.Column(AREADATAINPUT, vertical_alignment='c')]], vertical_alignment='c', title='')],
         [sg.Frame(layout=[[sg.Column(AREADRONESHOW, vertical_alignment='c')]], vertical_alignment='c', title='')]]
 
     AREARIGHT = [[sg.Text('Drones Status Indicators', size=(20, 1))],
-            [sg.Text('Drone1'), LEDIndicator('_drone1_')],
-            [sg.Text('Drone2'), LEDIndicator('_drone2_')],
-            [sg.Text('Drone3'), LEDIndicator('_drone3_')],
-            [sg.Text('Drone4'), LEDIndicator('_drone4_')],
-            [sg.Button('Exit')]]
+                 [sg.Text('Drone1'), LEDIndicator('_drone1_')],
+                 [sg.Text('Drone2'), LEDIndicator('_drone2_')],
+                 [sg.Text('Drone3'), LEDIndicator('_drone3_')],
+                 [sg.Text('Drone4'), LEDIndicator('_drone4_')],
+                 [sg.Button('Exit')]]
 
     layout = [[sg.Column(AREALEFT), sg.Column(AREARIGHT)]]
     sg.theme('Dark Blue 3')
 
     WINDOW = sg.Window("DroneShow", layout, margins=(100, 50))
 
-
-
-    # thread1 = Thread(target=readCSV,name='read_csv',args=csvpath)
-    print("The number of threads currently active: ",threading.active_count())
-
-
+    drone_agg = None
     while True:  # Event Loop
         event, value = WINDOW.read(timeout=400)
         if event == 'Exit' or event == sg.WIN_CLOSED:
@@ -129,22 +139,36 @@ def main():
         if event == 'START':
             try:
                 csvpath = value['-CSV-']
-                thread1 = Thread(target=readCSV, name='read_csv', args=csvpath)
-                thread1.start()
-                # print(csvpath)
-                thread1.join()
-                print("The number of threads currently active: ", threading.active_count())
-                # readCSV(csv)
+                [x,y] = readCSV(csvpath)
+                # print(x,y)
+                drone_show(WINDOW,x,y)
+                sleep(1)
+
+
             except Exception as e:
-                print('CSV READ ERROR')
+                print('ERROR')
         SetLED(WINDOW, '_drone1_', 'green' if inSafeArea(x1, y1) else 'red')
         SetLED(WINDOW, '_drone2_', 'green' if inSafeArea(x2, y2) else 'red')
         SetLED(WINDOW, '_drone3_', 'green' if inSafeArea(x3, y3) else 'red')
         SetLED(WINDOW, '_drone4_', 'green' if inSafeArea(x4, y4) else 'red')
 
+# drones initial location
+x1,y1 = 50,400
+x2,y2 = 150,400
+x3,y3 = 250,400
+x4,y4 = 350,400
+
+
+def main():
+
+    thread1 = Thread(target=gui,name='main thread')
+    thread1.run()
+
+    thread1.join()
 
 if __name__ == '__main__':
     main()
 
+# readCSV("data/loc.csv")
 
 # window.close()
