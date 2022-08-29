9169e602890707ebda38b976f5a4d4140824ae91
Sumiet Gore <sumeetgore96@gmail.com>
2022-08-29 12:32:37 +0300

    Added some info

--- ds_ws/src/DroneShow/DroneShow/wifi_connect.py
+++ ds_ws/src/DroneShow/DroneShow/wifi_connect.py
@@ -1,4 +1,8 @@
-"""A small script to connect Tello Drone to a wifi network. Please change the wifi ssid and passwod below."""
+"""
+A small script to connect Tello Drone to a wifi network. Please change the wifi ssid and passwod below.
+Connect to your tello drone using WiFi and then run the script.
+To reset the Tello drone into AP mode, please follow the Tello documentation.
+"""
 
 from lib.tellopy import Tello
 
