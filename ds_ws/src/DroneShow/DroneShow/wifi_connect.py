7450bff44c3df785eae9b101b95665c7eec1dec0
Sumiet Gore <sumeetgore96@gmail.com>
2022-08-29 12:30:08 +0300

    Removed unnecessary files and folders     Modified wifi connect to include some info

--- ds_ws/src/DroneShow/DroneShow/wifi_connect.py
+++ ds_ws/src/DroneShow/DroneShow/wifi_connect.py
@@ -1,9 +1,14 @@
+"""A small script to connect Tello Drone to a wifi network. Please change the wifi ssid and passwod below."""
+
 from lib.tellopy import Tello
+
+ssid = "Your WiFi SSID"
+password = "YourPassword"
 
 tello = Tello()
 
 tello.connect()
 
-tello.connect_to_wifi("HUAWEI-B525-CBE1", "JJJD6YQ0R88")
+tello.connect_to_wifi(ssid, password)
 
 tello.end()
